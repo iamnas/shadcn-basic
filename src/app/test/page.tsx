f1e0f673e548deb4dd7f9fd1b4d69cf8db370bb3
iamnas <iamnas.eth@gmail.com>
2024-09-14 00:13:19 +0530

    first commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+    return (
+        <div className="h-full flex justify-center flex-col items-center " >
+            <div>
+            TestPage os
+            </div>
+
+            <Button variant={"destructive"}>ShadCn button</Button>
+        </div>
+    )
+}
